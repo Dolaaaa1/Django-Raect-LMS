7068f7bcc22d44e5052f9de5c77008305a24dbe7
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2024-09-06 21:42:11 +0300

    Custom User and Profile Model In Django

--- project/settings.py
+++ project/settings.py
@@ -132,6 +132,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+AUTH_USER_MODEL = 'userauths.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
