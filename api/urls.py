f3efa3e43048bd08ca20aea5e3d1ebcd8404b0a8
Ahmed Adel <119899462+Dolaaaa1@users.noreply.github.com>
2024-09-16 17:55:33 +0300

    Email System using Django

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,13 @@ from rest_framework_simplejwt.views import TokenRefreshView
  
  
 urlpatterns = [
+    # Authentication Endpoints
+
     path("user/token/", api_views.MyTokenObtainPairView.as_view()),
     path("user/token/refresh/", TokenRefreshView.as_view()),
     path("user/register/", api_views.RegisterView.as_view()),
     path("user/password-reset/<email>/", api_views.PasswordResetEmailVerifyAPIView.as_view()),
-    path("user/password-change/", api_views.PasswordChangeAPIView.as_view()), 
- 
+    path("user/password-change/", api_views.PasswordChangeAPIView.as_view()),
+   
+
 ]
